df1b3a9555304d1f30db90687340b36c9adeb1e8
Daniel Siegle <daniel.siegle@gmail.com>
2014-04-25 16:42:02 -0400

    addition setup files

---
+++ octavesetup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#This is the octave setup file for an Ubuntu 12.04 LTS EC2 instance
+
+sudo apt-get install -y octave
---
+++ rstudiosetup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#This file directs the setup of RStuio server on an Ubuntu 12.04 LTS EC2 instance
+
