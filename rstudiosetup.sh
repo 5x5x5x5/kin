2c0e88868c555fc994564ded8c78157150d6e041
Daniel Siegle <daniel.siegle@gmail.com>
2014-04-25 17:48:39 -0400

    add rstudio setup

--- rstudiosetup.sh
+++ rstudiosetup.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 #This file directs the setup of RStuio server on an Ubuntu 12.04 LTS EC2 instance
 
+# Let's add a user
+sudo adduser rstudio
+
+#update
+sudo apt-get update
+
+#install r
+sudo apt-get install r-base
+
+#install rstudio
+sudo apt-get install gdebi-core
+sudo apt-get install libapparmor1 # required only for Ubuntu
+wget http://download2.rstudio.org/rstudio-server-0.98.507-amd64.deb
+sudo gdebi rstudio-server-0.98.507-amd64.deb
+
+# Just checking...
+sudo rstudio-server verify-installation
+
+# in browser go to http://ec2-gobbledygook-1.amazonaws.com:8787
