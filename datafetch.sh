9b1ad323f0bbc59f9950f7d5a2f471f663d43b68
Daniel Siegle <daniel.siegle@gmail.com>
2014-04-25 16:46:50 -0400

    added data pointer file

---
+++ datafetch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# this is the pointer to assay data stored on figshare
+
+mkdir data
+chmod 755 data
+cd data
+wget http://files.figshare.com/1471460/supermergewashdescriptors.txt
+wget http://http://files.figshare.com/1471458/5isozymedw1bin.txt
