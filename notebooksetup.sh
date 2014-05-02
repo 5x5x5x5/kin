d697a819d2051bf855d26b15631d1ce955fa60b3
5x5x5x5 <5x5x5x5@users.noreply.github.com>
2014-05-02 16:18:49 -0400

    Update notebooksetup.sh

--- notebooksetup.sh
+++ notebooksetup.sh
@@ -15,6 +15,9 @@ conda install anaconda # need to click yes - look for flags to skip this
 ## Default install does not include IPython 2 yet, so...
 conda update ipython
 
+##Additional package for cheminformatics
+sudo apt-get install python-rdkit librdkit1 rdkit-data
+
 ## Create nbserver profile
 ipython profile create nbserver
 ipython
